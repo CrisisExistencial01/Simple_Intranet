34edbff8756587a17f5989084288d90b9d1a0422
Cris <c.cheuquel01@ufromail.cl>
2023-11-29 17:31:27 -0300

    Cambios varios

--- Admin.py
+++ Admin.py
@@ -1,4 +1,5 @@
 import csv
+
 path_Usuarios = "Data/Users/Users.csv"
 path_Estudiantes = "Data/Users/Estudiantes.csv"
 path_Profesores = "Data/Users/Profesores.csv"
@@ -12,7 +13,15 @@ class color:
     RESET = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
-
+    def printOK(self, texto):
+        print(f"[{color.OKGREEN}*{color.RESET}] {texto}")
+    def printFail(self, texto):
+        print(f"[{color.FAIL}*{color.RESET}] {texto}")
+    def printWarning(self, texto):
+        print(f"[{color.WARNING}*{color.RESET}] {texto}")
+    def printBlue(self, texto):
+        print(f"[{color.BLUE}*{color.RESET}] {texto}")
+color = color()
 class Admin:
     # ADMINISTRACION DE ARCHIVOS CSV
     def find(self, parametro , lista):
@@ -49,16 +58,16 @@ class Admin:
         self.saveList(path, l)
     def update(self, path, data, new):
         # Actualiza un dato de un archivo csv
-        l = self.loadFile(path)
+        lista = self.loadFile(path)
         # l es la lista de tuplas retornadas por loadFile()
-        for dato in l:
+        for dato in lista:
             if data == data:
-                search = l.index(dato)
-                l[search] = new
-        self.saveList(path, l)
+                search = lista.index(dato)
+                lista[search] = new
+        self.saveList(path, lista)
     def add(self, path, data):
         # Agrega un dato a un archivo csv
-        l = self.loadFile(path)
+        lista = self.loadFile(path)
         # l es la lista de tuplas retornadas por loadFile()
         l.append(data)
         self.saveList(path, l)
@@ -68,15 +77,15 @@ class Admin:
         rut = input("Rut del nuevo usuario: ")
         nombre = input("Nombre del nuevo usuario: ")
         rol = input("Seleccione el tipo de usuario: \n(1) Estudiante\n(2) Profesor: ")
-        l = []
+        lista = []
         if rol == "1":
             user = (rut, rut, nombre, "Estudiante")
-            data = (rut, nombre, l[0:])
+            data = (rut, nombre, lista[0:])
             Intranet.estudiantes.append(data)
             self.save(path_Estudiantes, data)
         elif rol == "2":
             user = (rut, rut, nombre, "Profesor")
-            data = (rut, nombre, l[0:])
+            data = (rut, nombre, lista[0:])
             Intranet.profesores.append(data)
             self.save(path_Profesores, data)
         else:
@@ -105,5 +114,42 @@ class Admin:
             print(f"[{color.OKGREEN}*{color.RESET}]Usuario agregado exitosamente")
 
         else:
-            print(f"[{color.FAIL}*{color.RESET}]Opción no válida, intente nuevamente")
-              
+            print(f"[{color.FAIL}*{color.RESET}] Opción no válida, intente nuevamente")
+    def deleteUser(self, Intranet):
+        rut = input("Ingrese el rut del usuario a eliminar: ")
+        user = self.find(rut, Intranet.users)
+        if user == None:
+            #print(f"[{color.FAIL}*{color.RESET}] Usuario no encontrado")
+            color.printFail("Usuario no encontrado")
+        else:
+            Intranet.users.pop(user)
+            self.saveList(path_Usuarios, Intranet.users)
+            print(f"[{color.OKGREEN}*{color.RESET}] Usuario eliminado exitosamente")
+    def updateUser(self, Intranet):
+        rut = input("Ingrese el rut del usuario a actualizar: ")
+        user = self.find(rut, Intranet.users)
+        if user == None:
+            print(f"[{color.FAIL}*{color.RESET}] Usuario no encontrado")
+        else:
+            opt = int(input("Seleccione el dato a actualizar: \n(1) Rut\n(2) Nombre: \n(3)contraseña:\n(4) Tipo de usuario: "))
+            if opt == 1:
+                rut = input("Ingrese el nuevo rut del usuario: ")
+                Intranet.users[user][0] = rut
+                self.saveList(path_Usuarios, Intranet.users)
+                print(f"[{color.OKGREEN}*{color.RESET}] Usuario actualizado exitosamente")
+            elif opt == 2:
+                nombre = input("Ingrese el nuevo nombre del usuario: ")
+                Intranet.users[user][2] = nombre
+                self.saveList(path_Usuarios, Intranet.users)
+                print(f"[{color.OKGREEN}*{color.RESET}] Usuario actualizado exitosamente")
+            elif opt == 3:
+                password = input("Ingrese la nueva contraseña del usuario: ")
+                Intranet.users[user][1] = password
+                self.saveList(path_Usuarios, Intranet.users)
+                print(f"[{color.OKGREEN}*{color.RESET}] Usuario actualizado exitosamente")
+            elif opt == 4:
+                rol = input("Ingrese el nuevo tipo de usuario: ")
+                Intranet.users[user][3] = rol
+                self.saveList(path_Usuarios, Intranet.users)
+                print(f"[{color.OKGREEN}*{color.RESET}] Usuario actualizado exitosamente")
+
